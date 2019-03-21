fc0f6a73b5edecf3c875f463ea239eb252c332f9
JamesSG0823 <james.grice823@gmail.com>
2019-03-21 14:28:51 -0400

    Added comments

--- oddities.java
+++ oddities.java
@@ -16,17 +16,18 @@ public class oddities
 			{
 				isEven = true;
 				System.out.println("Number is even.");
-			}
+			}//if
+
 			else
 			{
 				System.out.println("Number is odd.");
-			}
+			}//else
 
 			if(numberToCheck == 0)
 			{
 				System.out.println("Not even or odd");
 				isEven = false;
-			}
+			}//if
 		}//for
 	}//main method
 }//class
